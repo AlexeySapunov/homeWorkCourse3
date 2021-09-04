218b7842078ec95fb2df465e09072d07c4791190
Alexey <blacka8607@rambler.ru>
2021-09-04 17:25:39 +0300

    Changes to the lesson1 option 2

--- src/main/java/ru/geekbrains/lesson1/Main.java
+++ src/main/java/ru/geekbrains/lesson1/Main.java
@@ -4,13 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Main {
+public class Main<T> {
     public static void main(String[] args) {
 
         // 1. Написать метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа);
         final String[] strings = {"abc", "def"};
         System.out.println(Arrays.toString(strings));
-        swap(strings, 0, 1 );
+        final Main<Object> main = new Main<>();
+        main.swap(strings, 0, 1);
+        System.out.println(Arrays.toString(strings));
 
         // 2. Написать метод, который преобразует массив в ArrayList;
 
@@ -18,15 +20,14 @@ public class Main {
         System.out.println(arrList);
     }
 
-    public static void swap(final String[] str, int a, int b) {
-        final String strings = str[a];
-        str[a] = str[b];
-        str[b] = strings;
-        System.out.println(Arrays.toString(str));
+    public void swap(final T[] arr, int a, int b) {
+        final T swap = arr[a];
+        arr[a] = arr[b];
+        arr[b] = swap;
     }
 
     public static <T> ArrayList<T> arrayToList(T[] arrList) {
-        return new ArrayList<T>(Arrays.asList(arrList));
+        return new ArrayList<>(Arrays.asList(arrList));
     }
 
 }
